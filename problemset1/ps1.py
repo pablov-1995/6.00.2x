0417321b39a9e892ee13baa81dcfcf0e1eb573ae
Pablo V <pablovizan@gmail.com>
2020-01-28 18:07:26 +0100

    Reviewed and changed Problem Set 1

--- problemset1/ps1.py
+++ problemset1/ps1.py
@@ -3,6 +3,7 @@
 
 from problemset1 import ps1_partition as pt
 import time
+import operator
 
 #================================
 # Part A: Transporting Space Cows
@@ -54,23 +55,45 @@ def greedy_cow_transport(cows,limit=10):
     transported on a particular trip and the overall list containing all the
     trips
     """
-    import operator
+
+    # Turn dictionary into ordered list of tuples
     elements = sorted(cows.items(), key=operator.itemgetter(1))
     elements.reverse()
-    reslis = []
-    elements_base = elements.copy()
-    while elements_base:
-        elements = elements_base.copy()
-        trip = []
-        weight = 0
-        for cow in elements:
-            if cow[1] <= (limit - weight):
-                trip.append(cow[0])
-                weight += cow[1]
-                elements_base.remove(cow)
-        reslis.append(trip)
-    return reslis
 
+    # Filter out those cows that are too heavy to fly in the spaceship
+    elements = [element for element in elements if element[1] <= limit]
+
+    # Define trip function
+    def trip(cows, available_capacity):
+        """
+        Return a list containing the name of the cows that will do the next trip in the spaceship.
+        :param cows: list of tuples, each tuple representing a cow that still has to do the trip, whose name (string)
+        is the first parameter of the tuple and whose weight (int) is the second.
+        :param available_capacity: int, maximum weight that the spaceship can carry
+        :return: trip: list of strings, names of the cows that will go in the spaceship
+        """
+        travellers = []
+        for cow in cows:
+            heaviest_cow_name = cow[0]
+            heaviest_cow_weight = cow[1]
+            if not available_capacity:
+                break
+            elif heaviest_cow_weight <= available_capacity:
+                travellers.append(heaviest_cow_name)
+                available_capacity -= heaviest_cow_weight
+
+        return travellers
+
+    # Initialise the variable trips, which will contain every trip done by the spaceship and the cows contained in it
+    trips = []
+
+    # Keep calculating new trips until the remaining cows equal zero
+    while elements:
+        next_trip = trip(elements, limit)
+        trips.append(next_trip)
+        elements = [cow for cow in elements if cow[0] not in next_trip]
+
+    return trips
 
 # Problem 2
 def brute_force_cow_transport(cows,limit=10):
@@ -93,26 +116,33 @@ def brute_force_cow_transport(cows,limit=10):
     transported on a particular trip and the overall list containing all the
     trips
     """
-    import operator
+    # Turn dictionary into ordered list of tuples
     elements = sorted(cows.items(), key=operator.itemgetter(1))
     elements.reverse()
-    pot_ret = []
-    for partition in pt.get_partitions(elements):
-        flag = True
-        newval = partition
-        for trip in partition:
-            weight = sum([cow[1] for cow in trip])
-            if weight > limit:
-                flag = False
-                break
-        if flag:
-            retval = []
-            for el in newval:
-                lista = [tup[0] for tup in el]
-                retval.append(lista)
-            pot_ret.append(retval)
-    min_len = min([len(ret) for ret in pot_ret])
-    return [ret for ret in pot_ret if len(ret) == min_len][0]
+
+    # Define check_trips function
+    def check_trips(partition):
+        """
+        Check if any trip in the partition is impossible to do due to the total weight of all the cows
+        exceeding the weight limit of the spaceship.
+        :param partition: list of lists, combination of trips that is being checked
+        :return: True if all the trips are doable; False if not.
+        """
+        valid_trip = lambda x: sum([cow[1] for cow in x]) <= limit
+        return all([valid_trip(trip) for trip in partition])
+
+    # Define partitions, a generator that will yield every possible combination of trips
+    partitions = pt.get_partitions(elements)
+
+    # Filter out those combination that do not pass the check, and sort the remaining ones by length in ascending order
+    valid_partitions = [partition for partition in partitions if check_trips(partition)]
+    valid_partitions.sort(key=len)
+
+    # Return the first combination of the list (it could as well be any other with the same length)
+    return_partition = valid_partitions[0]
+    cows_names = [[cow[0] for cow in trip] for trip in return_partition]
+
+    return cows_names
 
 
 # Problem 3
@@ -133,7 +163,7 @@ def compare_cow_transport_algorithms(cows):
     greedy_cow_transport(cows)
     greedy_time = time.time() - start_time
     print(f"Number of trips using greedy algorithm: {len(greedy_cow_transport(cows))}")
-    print(f"Time it takes for the method to run: {greedy_time}")
+    print(f"Time it takes for the greedy method to run: {greedy_time}\n")
     start_time = time.time()
     brute_force_cow_transport(cows)
     brute_time = time.time() - start_time
